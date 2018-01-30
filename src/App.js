0d3f416ab4ceef2dedfcf2170c541e389e30009a
Comicome <come.dlvl@gmail.com>
2018-01-30 14:30:08 +0100

    Renvoie les noms des épisodes quand on tape la série associé dans le champ

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends React.Component {
             seriesList: [],
             seriesEpisodesList: [],
             value: '',
-            output: '',
-            matchingSerie: ''
+            output: [],
+            outputSeries: '',
+            matchingSerie: '',
         };
         this.onChange = this.onChange.bind(this)
     }
@@ -25,7 +26,6 @@ class App extends React.Component {
                     .then(seriesEpisodesListDepuisFichier => {
                         this.setState({seriesEpisodesList: seriesEpisodesListDepuisFichier});
                     })
-
             })
             .catch(function (error) {
                 console.log(error);
@@ -33,45 +33,42 @@ class App extends React.Component {
             .then(function () {
                 /*alert("j'ai fait ce que j'ai pu");*/
             });
-
     }
 
     onChange(event) {
+        //on met la valeur du champ dans la variable value
         this.setState({
             value: event.target.value
         });
         let seriesTitleSearchValue = this.state.value
             .toLowerCase() // transforme en minuscule
             .trim(); // enleve les blancs au debut et fin de chaine
-
             /*
                 si le champ recupéré est vide ben ca sert a rien de faire
              */
         if (seriesTitleSearchValue !== "") {
-
-            console.log(seriesTitleSearchValue);
-
             let matchingSeries = this.state.seriesList.filter(
-                a => a.seriesName.toLowerCase().trim().indexOf(seriesTitleSearchValue) > -1);
+                (a) => (a.seriesName.toLowerCase().trim().indexOf(seriesTitleSearchValue) > -1)
+            );
             // boucle sur le tableau des series qui correspondent
             for (this.state.matchingSerie of matchingSeries) {
-                this.setState({output: this.state.matchingSerie.seriesName});
+                this.setState({outputSeries: this.state.matchingSerie.seriesName});
+
+
+                let matchingSerieEpisodesLists = this.state.seriesEpisodesList.filter(
+                    b => b.serie_id === this.state.matchingSerie.id
+                );
+                /*
+                    on sait que dans tous les cas un tableau est retourné
+                    mais on ne souhaite que la premiere liste de listes
+                */
+                if (matchingSerieEpisodesLists.length) {
+                    let matchingSerieEpisodesList = matchingSerieEpisodesLists[0];
+                    // boucle sur le tableau des listes d'episode qui correspondent
+                    for (let episode of matchingSerieEpisodesList.episodes_list) {
+                        this.state.output += episode.episodeName;
+                    }
             }
-            let matchingSerieEpisodesLists = this.state.seriesEpisodesList.filter(
-                b => b.serie_id === this.state.matchingSerie.id
-            );
-
-            /*
-                on sait que dans tous les cas un tableau est retourné
-                mais on ne souhaite que la premiere liste de listes
-            */
-            if (matchingSerieEpisodesLists.length) {
-                let matchingSerieEpisodesList = matchingSerieEpisodesLists[0];
-
-                // boucle sur le tableau des listes d'episode qui correspondent
-                for (let episode of matchingSerieEpisodesList.episodes_list) {
-                    this.setState({output: episode.episodeName});
-                }
             }
         }
     }
@@ -85,6 +82,11 @@ class App extends React.Component {
                         <input
                             type="text" value={this.state.value} onChange={this.onChange}/>
                     </label>
+                    {this.state.value !== "" ?
+                    <h2>Nom de la série : {this.state.outputSeries}</h2>
+                        : <h2>Chargement...</h2>
+                    }
+                    <h3>Episodes : </h3>
                     {this.state.value !== "" ?
                         <li>{this.state.output}</li>
                                             :
